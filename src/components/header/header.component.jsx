8a59f63bfa6da85f606368d0f2303cd2f95d0905
juanherz <juan.herz.universidad@gmail.com>
2021-09-14 23:53:26 -0500

    integrated firebase to the site, created a sign in page with sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter, Link } from "react-router-dom";
 import './header.styles.scss';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({currentUser}) => (
 
     <div className='header'>
         <BrowserRouter>
@@ -22,6 +23,12 @@ const Header = () => (
                 <Link className='option' to='/shop'>
                     CONTACT
                 </Link>
+                {
+                    currentUser ? 
+                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className="option" to='/signin'>SIGN IN</Link>
+                }
             </BrowserRouter>
             
         </div>
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyCTiX8US1r0L0QKV5kSyMuLAyDkbElNKd4",
+    authDomain: "e-commerce-db-32a4f.firebaseapp.com",
+    projectId: "e-commerce-db-32a4f",
+    storageBucket: "e-commerce-db-32a4f.appspot.com",
+    messagingSenderId: "400972094885",
+    appId: "1:400972094885:web:1cb6d1f7d759a57fda9c4c",
+    measurementId: "G-1BQRY82QPQ"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
